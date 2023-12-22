df896d7662ebc834fb619e751322b682d5881ae1
Ron42 <leeronald42@gmail.com>
2023-12-22 17:17:11 -0600

    Add support for scheduled tasks

---
+++ src/main/java/org/rsa/task/TaskObject.java
@@ -0,0 +1,27 @@
+package org.rsa.task;
+
+import lombok.Getter;
+import net.dv8tion.jda.api.JDA;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+
+public abstract class TaskObject {
+    @Getter
+    private final String name;
+    protected JDA jda;
+    protected ScheduledFuture<?> scheduledTask;
+    protected ScheduledExecutorService scheduler;
+
+    public TaskObject(String name, JDA jda, ScheduledExecutorService scheduler) {
+        this.name = name;
+        this.jda = jda;
+        this.scheduler = scheduler;
+    }
+
+    protected abstract void execute();
+    public abstract void startTask();
+    public void stopTask() {
+        scheduledTask.cancel(true);
+    }
+}
