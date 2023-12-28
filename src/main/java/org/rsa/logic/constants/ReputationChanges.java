b663f59217fe566d43077797675776b2677e9011
AlphaBlade01 <ayaanuppal99@gmail.com>
2023-12-28 23:32:53 +0000

    Removed redundant COMMENT_UPVOTE_RECEIVED field.

--- src/main/java/org/rsa/logic/constants/ReputationChanges.java
+++ src/main/java/org/rsa/logic/constants/ReputationChanges.java
@@ -3,7 +3,6 @@ package org.rsa.logic.constants;
 public class ReputationChanges {
     public static final int POST_UPVOTE_RECEIVED = 5;
     public static final int POST_DOWNVOTE_RECEIVED = -2;
-    public static final int COMMENT_UPVOTE_RECEIVED = 1;
     public static final int ANSWER_ACCEPT_RECEIVED = 15;
     public static final int ANSWER_ACCEPT_GIVEN = 2;
     public static final int POST_DOWNVOTE_GIVEN = -1;
