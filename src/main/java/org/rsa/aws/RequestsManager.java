2fc3a5691cc7079a8c396cb781f157e20ad0c7fc
AlphaBlade01 <ayaanuppal99@gmail.com>
2023-12-28 23:32:00 +0000

    Made table hidden, Added fetching single item functionality, Added ability to enqueue update using model.

--- src/main/java/org/rsa/aws/RequestsManager.java
+++ src/main/java/org/rsa/aws/RequestsManager.java
@@ -1,20 +1,24 @@
 package org.rsa.aws;
 
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.model.UpdateItemEnhancedRequest;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.*;
 
 import java.util.LinkedList;
+import java.util.Optional;
 import java.util.Queue;
 
 
 public class RequestsManager<T> {
     private final Queue<UpdateItemEnhancedRequest<T>> _updateRequestsQueue = new LinkedList<>();
-    private boolean _processingUpdateRequests = false;
     private final DynamoDbTable<T> _table;
+    private final Class<T> _classType;
+    private boolean _processingUpdateRequests = false;
 
-    public RequestsManager(DynamoDbTable<T> table)
+    public RequestsManager(String tableName, Class<T> type)
     {
-        this._table = table;
+        this._table = DynamoDB.GetDynamoTable(tableName, TableSchema.fromBean(type));
+        this._classType = type;
     }
 
     private void processUpdateRequests()
@@ -29,10 +33,29 @@ public class RequestsManager<T> {
         processUpdateRequests();
     }
 
-    public void enqueue(UpdateItemEnhancedRequest<T> request)
+    public void enqueueItemUpdate(T item)
+    {
+        UpdateItemEnhancedRequest<T> request = UpdateItemEnhancedRequest.builder(_classType)
+                .item(item)
+                .build();
+        enqueueUpdateRequest(request);
+    }
+
+    public void enqueueUpdateRequest(UpdateItemEnhancedRequest<T> request)
     {
         _updateRequestsQueue.add(request);
         if (_processingUpdateRequests) return;
         processUpdateRequests();
+    }
+
+    public Optional<Page<T>> fetchSingleItem(QueryConditional queryConditional)
+    {
+        QueryEnhancedRequest request = QueryEnhancedRequest.builder()
+            .queryConditional(queryConditional)
+            .limit(1)
+            .scanIndexForward(false)
+            .build();
+        PageIterable<T> pages = _table.query(request);
+        return pages.stream().findFirst();
     }
 }
