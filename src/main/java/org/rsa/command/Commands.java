734a78353b71089549cd0dfbe08003fb6ecd7441
TinyPandas <chris.barwick16@gmail.com>
2024-03-23 04:21:46 -0700

    Register check-out command.

--- src/main/java/org/rsa/command/Commands.java
+++ src/main/java/org/rsa/command/Commands.java
@@ -1,9 +1,6 @@
 package org.rsa.command;
 
-import org.rsa.command.commands.CheckInCommand;
-import org.rsa.command.commands.CompileCommand;
-import org.rsa.command.commands.ConfigureCommand;
-import org.rsa.command.commands.ResolveCommand;
+import org.rsa.command.commands.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,6 +13,7 @@ public class Commands {
         System.out.println("Loading commands.");
 
         addCommandObject(new CheckInCommand());
+        addCommandObject(new CheckOutCommand());
         addCommandObject(new CompileCommand());
         addCommandObject(new ConfigureCommand());
         addCommandObject(new ResolveCommand());
