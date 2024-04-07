0de8b27652223d1e1e3b11feb15970b3b1eebb61
Ron <leeronald42@gmail.com>
2024-04-06 21:24:06 -0500

    DevforumUpdateTask and DevforumUpdate command group

--- src/main/java/org/rsa/task/tasks/DevforumUpdateTask.java
+++ src/main/java/org/rsa/task/tasks/DevforumUpdateTask.java
@@ -1,20 +1,30 @@
 package org.rsa.task.tasks;
 
 import net.dv8tion.jda.api.JDA;
+import org.rsa.discourse.DiscourseAPIHelper;
+import org.rsa.discourse.models.CategoryDetailsModel;
+import org.rsa.exception.ApiException;
 import org.rsa.task.TaskObject;
+
+import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class DevforumUpdateTask extends TaskObject {
     private static final String TASK_NAME = "DevforumUpdateTask";
-    private static final long UPDATE_INTERVAL = 30;
-    private static final TimeUnit TIME_UNIT = TimeUnit.SECONDS;
+    private static final long UPDATE_INTERVAL = 5;
+    private static final TimeUnit TIME_UNIT = TimeUnit.MINUTES;
 
     public DevforumUpdateTask(JDA jda, ScheduledExecutorService scheduler) {
         super(TASK_NAME, jda, scheduler);
     }
     protected void execute() {
-
+        try {
+            Map<String, CategoryDetailsModel> categories = DiscourseAPIHelper.fetchAllCategoryInformation();
+        } catch(ApiException | IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     public void startTask() {
