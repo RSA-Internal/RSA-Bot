ee16b8caf2246dec273f614b3ddbdbf204b7d5f6
Ron <leeronald42@gmail.com>
2024-04-26 09:22:58 -0500

    Add SubscribeSubcommand and UpdateTask (WIP)

--- src/main/java/org/rsa/command/subcommands/configure/devforumUpdatesSubcommand/SubscribeSubcommand.java
+++ src/main/java/org/rsa/command/subcommands/configure/devforumUpdatesSubcommand/SubscribeSubcommand.java
@@ -1,20 +1,11 @@
 package org.rsa.command.subcommands.configure.devforumUpdatesSubcommand;
 
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
 import org.rsa.command.SubcommandObject;
-import org.rsa.discourse.DiscourseAPIHelper;
-import org.rsa.discourse.models.CategoryDetailsModel;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class SubscribeSubcommand extends SubcommandObject {
     public SubscribeSubcommand() {
@@ -24,22 +15,5 @@ public class SubscribeSubcommand extends SubcommandObject {
 
     public void handleSubcommand(@NotNull SlashCommandInteractionEvent event, @NotNull Guild guild) {
         event.reply("Edited subscribed category").setEphemeral(true).queue();
-    }
-
-    private class AutoCompleteHandler extends ListenerAdapter {
-        @Override
-        public void onCommandAutoCompleteInteraction(@NotNull CommandAutoCompleteInteractionEvent event) {
-            if (event.getName().equals("subscribe") && event.getFocusedOption().getName().equals("category")) {
-                String userInput = event.getFocusedOption().getValue();
-                Map<String, CategoryDetailsModel> categoryDetailsModelMap = DiscourseAPIHelper.getLatestCategoriesData();
-                List<Command.Choice> choices = categoryDetailsModelMap.values().stream()
-                        .filter(category -> category.getName().toLowerCase().startsWith(userInput.toLowerCase()))
-                        .limit(OptionData.MAX_CHOICES)
-                        .map(category -> new Command.Choice(category.getName(), category.getId()))
-                        .collect(Collectors.toList());
-
-                event.replyChoices(choices).queue();
-            }
-        }
     }
 }
--- src/main/java/org/rsa/task/tasks/DevforumUpdateTask.java
+++ src/main/java/org/rsa/task/tasks/DevforumUpdateTask.java
@@ -1,12 +1,18 @@
 package org.rsa.task.tasks;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
-import org.rsa.discourse.DiscourseAPIHelper;
-import org.rsa.discourse.models.CategoryDetailsModel;
-import org.rsa.exception.ApiException;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import org.jetbrains.annotations.NotNull;
+import org.rsa.net.apis.discourse.DiscourseAPI;
+import org.rsa.net.apis.discourse.domain.Category;
+import org.rsa.net.apis.discourse.domain.Topic;
+import org.rsa.net.apis.roblox.RobloxAPI;
+import org.rsa.net.apis.roblox.models.users.MultiGetUserByNameModel;
 import org.rsa.task.TaskObject;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -15,19 +21,90 @@ public class DevforumUpdateTask extends TaskObject {
     private static final String TASK_NAME = "DevforumUpdateTask";
     private static final long UPDATE_INTERVAL = 5;
     private static final TimeUnit TIME_UNIT = TimeUnit.MINUTES;
+    private static final String AUTHOR_FORMAT = "Developer Forum | Roblox | %s";
+    private static final String FOOTER_FORMAT = "Posted by %s";
+    private static final String TRUNCATE_FORMAT_STRING = "\n\n[(Read more...)](%s)";
+    private static String THUMBNAIL_URL = "https://devforum-uploads.s3.dualstack.us-east-2.amazonaws.com/uploads/original/5X/c/3/2/5/c325b8f46fd0b3c5b418b005b66c7af661539d44.png";
+    private static final int MAX_DESCRIPTION_LENGTH_CHAR = 500;
 
     public DevforumUpdateTask(JDA jda, ScheduledExecutorService scheduler) {
         super(TASK_NAME, jda, scheduler);
+
+        try {
+            THUMBNAIL_URL = DiscourseAPI.fetchSiteBasicInfo().logo_url(); // Try and grab the latest thumbnail from forum
+        } catch(IOException e) {
+            System.err.println(e.getMessage());
+        }
     }
+
     protected void execute() {
         try {
-            Map<String, CategoryDetailsModel> categories = DiscourseAPIHelper.fetchAllCategoryInformation();
-        } catch(ApiException | IOException e) {
+            Map<String, Category> categories = DiscourseAPI.fetchAllCategoryInformation();
+            categories.forEach((categoryId, categoryDetails) -> {
+                try {
+                    Topic topicResponse = DiscourseAPI.fetchLatestPostInCategory(categoryId);
+                    MessageEmbed messageEmbed = createEmbed(topicResponse, categoryDetails);
+                    jda.getTextChannelById("1187145914343764129").sendMessageEmbeds(messageEmbed).queue();
+                } catch(IOException e) {
+                    System.err.println(e.getMessage());
+                }
+            });
+        } catch(IOException e) {
             System.err.println(e.getMessage());
         }
     }
 
     public void startTask() {
         scheduledTask = scheduler.scheduleAtFixedRate(this::execute, 0, UPDATE_INTERVAL, TIME_UNIT);
+    }
+
+    private MessageEmbed createEmbed(Topic topicDetails, Category postCategoryDetails) {
+        String baseUrl = DiscourseAPI.getBASE_URL();
+        String topicUrl = baseUrl + "/t/" + topicDetails.id();
+        String categoryUrl = baseUrl + "/c/" + postCategoryDetails.id();
+        String description = trimDescription(topicDetails, topicUrl);
+
+        EmbedBuilder embedBuilder = new EmbedBuilder()
+                .setAuthor(String.format(AUTHOR_FORMAT, postCategoryDetails.name()), categoryUrl)
+                .setTitle(topicDetails.title())
+                .setDescription(description)
+                .setUrl(topicUrl)
+                .setThumbnail(THUMBNAIL_URL)
+                .setColor(postCategoryDetails.categoryColor())
+                .setTimestamp(topicDetails.createdAt());
+
+        try {
+            // Grab profile image URL from thumbnails endpoint by getting ID from username
+            MultiGetUserByNameModel.User user = RobloxAPI.Users.multiGetUserByName(List.of(topicDetails.author()), true).data().get(0);
+            String imgUrl = RobloxAPI.Thumbnails.getAvatarHeadshot(List.of(user.id()), "50x50", "Png", false).data().get(0).imageUrl();
+            embedBuilder.setFooter(String.format(FOOTER_FORMAT, topicDetails.author()), imgUrl);
+        } catch(IOException | IndexOutOfBoundsException e) {
+            System.err.println(e.getMessage());
+            embedBuilder.setFooter(String.format(FOOTER_FORMAT, topicDetails.author()));
+        }
+
+        if (topicDetails.imageUrl() != null) {
+            embedBuilder.setImage(topicDetails.imageUrl());
+        }
+
+        return embedBuilder.build();
+    }
+
+    private static @NotNull String trimDescription(Topic topicDetails, String topicUrl) {
+        String description = topicDetails.parsedContent();
+
+        // Truncate description after a space, if possible
+        if (description.length() > MAX_DESCRIPTION_LENGTH_CHAR) {
+            int end = MAX_DESCRIPTION_LENGTH_CHAR - 3;
+            int lastSpace = description.lastIndexOf(' ', end);
+            if (lastSpace < 0) {
+                description = description.substring(0, end);
+            } else {
+                description = description.substring(0, lastSpace);
+            }
+
+            description += String.format(TRUNCATE_FORMAT_STRING, topicUrl);
+        }
+        return description;
     }
 }
